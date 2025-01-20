5132d5feaecadda89a8b84b350f5704b5b46ecf9
Johnson Chin <johnsonchin1009@gmail.com>
2025-01-21 07:46:29 +0800

    feat: update layout.tsx and page.tsx

---
+++ README.md
@@ -0,0 +1,4 @@
+Main Functions:
+
+1. Auto Scrap Data from Event Websites
+2. Update on DB with the latest data
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "Hive | The Place to Bee",
+  description: "Made with Love by 0x_Johnson",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
