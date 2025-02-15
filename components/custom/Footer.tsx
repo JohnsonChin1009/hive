1ab1ef9683ba2b7b358c27c3b780d8af189087d1
Johnson Chin <johnsonchin1009@gmail.com>
2025-02-15 18:44:57 +0800

    feat: updated Footer with responsive

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { GithubIcon } from "@/components/ui/github";
+
 import Image from "next/image";
+
 export default function Footer() {
   return (
     <>
-      <footer className="px-4 md:px-[60px] py-4 bg-[#1C1C1C] text-white flex justify-between items-center">
+      <footer className="px-4 md:px-[60px] py-4 bg-[#1C1C1C] text-white flex justify-between items-center text-center gap-2 md:gap-0">
         <div className="text-[12px] md:text-[16px] font-medium">
-          <Image src="/hive-logo-white.png" alt="logo" width={80} height={80} />
+          <div className="relative w-[60px] h-[40px] md:w-[80px] md:h-[80px]">
+            <Image
+              src="/hive-logo-white.png"
+              alt="logo"
+              fill
+              objectFit="contain"
+            />
+          </div>
         </div>
-        <div className="text-[12px] md:text-[16px]">
+        <div className="text-[10px] md:text-[16px]">
           made with 🤍 by{" "}
           <a
             href="https://github.com/JohnsonChin1009"
@@ -17,8 +26,8 @@ export default function Footer() {
             className="underline"
           >
             johnson
-          </a>
-          {" "} and {" "}
+          </a>{" "}
+          and{" "}
           <a
             href="https://junshen.vercel.app"
             target="_blank"
