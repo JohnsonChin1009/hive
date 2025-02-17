b9addca255b64cce129012ab6de48de1efd31813
Johnson Chin <johnsonchin1009@gmail.com>
2025-02-17 14:02:23 +0800

    fix: removed extra button on eventCard during mobile view

--- components/custom/EventCard.tsx
+++ components/custom/EventCard.tsx
@@ -22,15 +22,16 @@ export default function EventCard({ event }: EventCardProps) {
   return (
     <>
       {/* Card Border */}
-      <div className="w-full sm:w-[70vw] md:w-[45vw] lg:w-[30vw] xl:w-[22vw] 2xl:w-[20vw] max-w-[340px]
-                    md:min-h-[450px] 
-                    px-3 sm:px-4 py-3 sm:py-4 
-                    flex flex-col gap-3 sm:gap-4 
-                    items-center border-[2px] border-[#1C1C1C] 
+      <div
+        className="w-full sm:w-[70vw] md:w-[45vw] lg:w-[30vw] xl:w-[22vw] 2xl:w-[20vw] max-w-[340px]
+                    md:min-h-[450px]
+                    px-3 sm:px-4 py-3 sm:py-4
+                    flex flex-col gap-3 sm:gap-4
+                    items-center border-[2px] border-[#1C1C1C]
                     dark:border-[#adadad] rounded-xl md:rounded-3xl
-                    bg-white dark:bg-[#1C1C1C]/10 
-                    dark:backdrop-blur-md justify-between">
-
+                    bg-white dark:bg-[#1C1C1C]/10
+                    dark:backdrop-blur-md justify-between"
+      >
         {/* Event Banner */}
         <div className="relative w-full h-[180px] sm:h-[200px] lg:h-[210px] 2xl:h-[250px]">
           <Image
@@ -62,14 +63,12 @@ export default function EventCard({ event }: EventCardProps) {
                 {formatDate(event.date)}, {event.time}
               </span>
             </div>
-            
+
             <div className="flex items-center gap-2 dark:text-[#ffffff]">
               <div className="w-4 sm:w-5 h-4 sm:h-5">
                 <FaLocationDot className="text-base sm:text-lg" />
               </div>
-              <span className="flex-1 truncate">
-                {event.venue}
-              </span>
+              <span className="flex-1 truncate">{event.venue}</span>
             </div>
 
             <div className="flex gap-2 flex-wrap">
@@ -88,11 +87,6 @@ export default function EventCard({ event }: EventCardProps) {
         {/* Slide Button */}
         <div className="w-full">
           <EventButton eventURL={event.url} />
-        </div>
-        <div className="w-full block lg:hidden">
-          <button className="w-full border-2 border-[#1C1C1C] dark:border-[#adadad] rounded-lg text-black text-base font-medium dark:text-white p-2" onClick={() => window.open(event.url, "_blank")}>
-            View Event
-          </button>
         </div>
       </div>
     </>
